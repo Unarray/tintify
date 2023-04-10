8bb3fb16bed8b602ddae7c686de764d95d0ca636
Verre2OuiSki <verre2ouiski@gmail.com>
2023-04-10 12:26:33 +0200

    Add new formatter `matrix`

--- src/formatters/formatter.ts
+++ src/formatters/formatter.ts
@@ -1,5 +1,5 @@
-import { forgroundRGBColor } from "$src/colors";
-import { RGB } from "$src/utils/color";
+import { EffectReset, forgroundRGBColor } from "$src/colors";
+import { RGB, isInRGBRange } from "$src/utils/color";
 import { charIndexes, concatCharIndexes, linearGradientIndex } from "./formatter.util";
 
 export const linearGradient = (message: string, start: RGB, end: RGB, ignoreSpaces = true): string => {
@@ -20,5 +20,25 @@ export const linearGradient = (message: string, start: RGB, end: RGB, ignoreSpac
     );
   }
 
-  return newMessage.join("");
+  return `${newMessage.join("")}${EffectReset.All}`;
+};
+
+export const matrix = (message: string, color: RGB, force = 100): string => {
+  if (!isInRGBRange(force)) throw new Error(`Invalid force. Value must be in [0, 255]: force=\`${force}\``);
+
+  let newMessage = "";
+
+  for (let i = 0; i < message.length; i++) {
+    const num = Math.floor(Math.random() * (force + 1 - 0) + 0); // Random number between 0 and force
+    // For each value cross-product
+    const colorNuance: RGB = {
+      red: Math.floor((color.red * (255 - num)) / 255),
+      green: Math.floor((color.green * (255 - num)) / 255),
+      blue: Math.floor((color.blue * (255 - num)) / 255)
+    };
+
+    newMessage += `${forgroundRGBColor(colorNuance)}${message[i]}`;
+  }
+
+  return `${newMessage}${EffectReset.All}`;
 };
